2b1a88264abac376818b9522689ffebccc5c75c5
Brian Pérez <bperezramos.contacto@gmail.com>
2020-03-02 10:13:25 +0000

    Despliegue de la Aplicación

---
+++ precache-manifest.2e489551186cf85ed64ae515aed539c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c41444c1b2876aef2f4ab60ffa826d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eac6e3ea6c270d11a494",
+    "url": "/static/css/main.e2a5f39b.chunk.css"
+  },
+  {
+    "revision": "83adf13c80c057227c45",
+    "url": "/static/js/2.a6accf8b.chunk.js"
+  },
+  {
+    "revision": "e71361cd22d9e155e529528b0ede5b32",
+    "url": "/static/js/2.a6accf8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eac6e3ea6c270d11a494",
+    "url": "/static/js/main.dc3de3fc.chunk.js"
+  },
+  {
+    "revision": "225df25a0067128d1f4e",
+    "url": "/static/js/runtime-main.2fba18c7.js"
+  },
+  {
+    "revision": "cb726edacac078a872ff074464786666",
+    "url": "/static/media/fondoIndex.cb726eda.png"
+  }
+]);
